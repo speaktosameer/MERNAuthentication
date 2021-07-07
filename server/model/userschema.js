664dbf7d93f71c71bcb35661d6deb6e06f93fca1
sameer <ekhlaque.ahmad@xtrastaff.com>
2021-07-07 08:01:37 +0545

    Storing user data

--- server/model/userschema.js
+++ server/model/userschema.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongooose = require('mongoose');
 
 const userSchema = new mongooose.Schema({
     name:{
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+require('../db/conn');
+const User =require('../model/userschema');
+
 router.get('/',(req,res)=>{
     res.send("Hello World from the server Router");
 });
 
 router.post('/register',(req,res)=>{
-    console.log(req.body);
-    res.json({message:req.body});
-    // response.send("mera register page");
+
+    const {name,email,phone,work,password,cpassword }=req.body;
+
+    if(!name || !email || !phone || !work || !password || !cpassword){
+        return res.status(422).json({error : "Please filled it properly"});
+    }
+
+    User.findOne({email:email})
+    .then((userExist)=>{
+        if(userExist){
+            return res.status(422).json({error : "Email already exist"});
+        }
+        const user= new User({name,email,phone,work,password,cpassword })
+        user.save().then(()=>{
+            res.status(201).json({message:"User suceesfully"});
+        }).catch((err)=> res.status(500).json({error:"Failed to register"}));
+    }).catch(err=>{console.log(err);});
 })
 
 module.exports = router;
