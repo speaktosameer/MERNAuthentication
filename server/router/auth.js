4185442a2d49e38975b82c43153b6f124d5776dc
sameer <ekhlaque.ahmad@xtrastaff.com>
2021-07-07 08:30:39 +0545

    Async

--- server/router/auth.js
+++ server/router/auth.js
@@ -8,7 +8,32 @@ router.get('/',(req,res)=>{
     res.send("Hello World from the server Router");
 });
 
-router.post('/register',(req,res)=>{
+//Using Promises
+
+// router.post('/register', (req,res)=>{
+
+//     const {name,email,phone,work,password,cpassword }=req.body;
+
+//     if(!name || !email || !phone || !work || !password || !cpassword){
+//         return res.status(422).json({error : "Please filled it properly"});
+//     }
+
+//     User.findOne({email:email})
+//     .then((userExist)=>{
+//         if(userExist){
+//             return res.status(422).json({error : "Email already exist"});
+//         }
+//         const user= new User({name,email,phone,work,password,cpassword })
+//         user.save().then(()=>{
+//             res.status(201).json({message:"User suceesfully"});
+//         }).catch((err)=> res.status(500).json({error:"Failed to register"}));
+//     }).catch(err=>{console.log(err);});
+// });
+
+
+//Async -await
+
+router.post('/register', async (req,res)=>{
 
     const {name,email,phone,work,password,cpassword }=req.body;
 
@@ -16,16 +41,22 @@ router.post('/register',(req,res)=>{
         return res.status(422).json({error : "Please filled it properly"});
     }
 
-    User.findOne({email:email})
-    .then((userExist)=>{
+    try{
+        const userExist =await User.findOne({email:email});
+
         if(userExist){
             return res.status(422).json({error : "Email already exist"});
         }
         const user= new User({name,email,phone,work,password,cpassword })
-        user.save().then(()=>{
-            res.status(201).json({message:"User suceesfully"});
-        }).catch((err)=> res.status(500).json({error:"Failed to register"}));
-    }).catch(err=>{console.log(err);});
-})
+        
+        await user.save();
+        res.status(201).json({message:"User suceesfully"});
+
+    } catch(err){
+        console.log(err);
+    }
+    
+});
+
 
 module.exports = router;
