86db1cf23b34eb7951e6bb4bbca82502d5b631a7
sameer <ekhlaque.ahmad@xtrastaff.com>
2021-07-08 08:41:09 +0545

    Frontend Implemaentation

---
+++ tutor/src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+    return (
+        <div>
+            <h1>Conatct Us</h1>
+        </div>
+    )
+}
+
+export default Contact
---
+++ tutor/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <p>WELCOME</p>
+            <h1>Welcome to Sameer Mern Project</h1>
+        </div>
+    )
+}
+
+export default Home
